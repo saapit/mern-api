070dfb9effbee96a4ca2fb36767ded7141ebe09c
sapit <syafiqshahrim19@gmail.com>
2021-02-20 08:53:32 +0800

    Setup basic router, request and response in NodeJS

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express'); // ini default from express
 // import express from 'express'    ..ni syntax Es6
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Hello server...');
-    console.log('Hello server...1');
-    console.log('Hello server...2');
+router.use('/products', (req, res, next) => {
+    console.log(' url: ', req.originalUrl);
+    console.log(' method: ', req.method);
+    res.json({name : "Muhammad Syafiq", email: "sapit@gmail.com"});
+    next(); // move to next method 
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({price: "RM 25.00"});
+    next();
+})
+
+app.use('/', router);
 
 app.listen(4000);
